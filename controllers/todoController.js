59ca879457409bb4eba9cf2372725d31a5905b34
Shailesh Shanbhag <Shailesh.Shanbhag@globalinsurance.co.in>
2020-04-13 21:46:48 +0530

    Todo list using express js. Add and delete implemented.

---
+++ controllers/todoController.js
@@ -0,0 +1,22 @@
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({extended:true})
+var data = [{item: 'get milk'}, {item:'some coding apps'}, {item:'walking'}];
+
+module.exports = function(app) {
+
+    app.get('/todo', function(req, res){
+        res.render('todo', {todos:data})
+    })
+
+    app.post('/todo', urlencodedParser, function(req,res){
+        data.push(req.body);
+        res.json(data);
+    })
+
+    app.delete('/todo/:item', function(req,res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g,'-') !== req.params.item;
+        })
+        res.json(data);
+    })
+};
